3459cb9574cd9f112b577f35c1cb6754946b438e
skanish0123 <skmacro92@gmail.com>
2026-04-10 00:38:03 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,1 +1,25 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="BUGSCAN",
+    version="2.0",
+    author="ANISH",
+    author_email="anish@bugscan.com",
+    description="Advanced Security Toolkit - Host Scanner, Port Scanner, IP Lookup & More",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/skanish0123/BUGSCAN",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Operating System :: Android",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "bugscan=bugscan.main:main",
+        ],
+    },
+)
